b6e9c568ee35527bad875759a87be71767543b38
Victor Capelli <victorbustocapelli@hotmail.com>
2020-07-10 15:24:40 -0300

    set the cloudinary key

--- app/controllers/pizzaria_controller.rb
+++ app/controllers/pizzaria_controller.rb
@@ -1,2 +1,12 @@
 class PizzariaController < ApplicationController
+
+  private
+
+  def set_pizzaria
+    @pizza = Pizzarium.find(params[:id])
+  end
+
+  def pizzaria_params
+    params_require(:pizzarium).permit(:name, :price, :descripton, :photo)
+  end
 end
--- app/models/pizzarium.rb
+++ app/models/pizzarium.rb
@@ -3,4 +3,6 @@ class Pizzarium < ApplicationRecord
   validates :name, presence: true
   validates :price, presence: true
   validates :description, presence: true
+
+  has_one_attached :photo
 end
